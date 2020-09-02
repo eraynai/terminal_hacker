8131860031557e8bf561be1fef0ce55b8eb70f31
eraynai <elli@cinehackers.com>
2020-09-02 16:29:55 -0400

    First Commit
    First Commit

---
+++ Assets/Scripts/Hacker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    //Game State
+    int level;
+    enum Screen { MainMenu, Password, Win};
+    Screen currentScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What Would You Like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter Your Selection:");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if(currentScreen == Screen.MainMenu)
+        {
+            NewMethod(input);
+        }
+    }
+
+    void NewMethod(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please Choose a valid level");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
